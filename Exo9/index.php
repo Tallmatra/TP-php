aa32764d13624c57fdb71c98a1f6ce8ee3c5a13c
Tallmatra <99328564+Tallmatra@users.noreply.github.com>
2022-02-14 14:20:05 +0000

    Delete index.php

--- Exo9/index.php
+++
@@ -1,6 +0,0 @@
-<form action="controller.php" method="post">
-    <label for="">Entrer un nombre :</label>
-    <input type="text" name="nombre"><br>
-    <input type="submit" value="Entrer" >
-
-</form>
